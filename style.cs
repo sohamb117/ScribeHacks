0f357ab722b163befa477c06b2efb31be8ea2f35
sohamb117 <sohamb117@gmail.com>
2019-10-20 14:52:42 -0700

    ScribeHacks
    Website for the ScribeHacks Hackathon

--- style.cs
+++ style.cs
@@ -3,3 +3,14 @@ body
 	background-color: rgb(37,39,77);
 	color: white;
 }
+.oceanImage
+{
+    position: absolute;
+    top: 0;
+    margin-left: auto;
+    margin-right: auto;
+    height: 50vh;
+    width: 50vh;
+    background-image: url("/Resources/ocean.png");
+    background-size: auto;
+}
